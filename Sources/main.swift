58157c5fcfaf1d4a1ddd9322bcd8cabf01bfc79e
akaMiWP <bangkokappdevelopment@gmail.com>
2025-02-08 17:03:46 +0700

    Added examples

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,4 +1,20 @@
 //
 import RxSwift
 
-print("Hello World")
+example(of: "Cold Observable") {
+    let just = Observable.just(1)
+    let of = Observable.of(1, 2, 3)
+    let from = Observable.from([4, 5])
+    
+    _ = just.subscribe { event in
+        print(">> Just:", event)
+    }
+    
+    _ = of.subscribe { event in
+        print(">> Of:", event)
+    }
+    
+    _ = from.subscribe { event in
+        print(">> From:", event)
+    }
+}
